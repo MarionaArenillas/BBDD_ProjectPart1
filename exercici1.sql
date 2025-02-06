44b42f26a18ec0a7919c44478493d477e1489c9b
MarionaArenillas <mariona.arenillas@gracia.lasalle.cat>
2025-02-06 12:15:56 +0100

    Corregir taula persona i crear taula equip_nacional

--- exercici1.sql
+++ exercici1.sql
@@ -9,13 +9,19 @@ DEFAULT COLLATE 'utf8mb4_spanish_ci';
 
 -- Creació de la taula Persona: 
 CREATE TABLE IF NOT EXISTS Persona (
-    DNI VARCHAR(9),
-    Nom VARCHAR(20),
-    Cognom1 VARCHAR(20),
-    Cognom2 VARCHAR(20),
-    Nacionalitat VARCHAR(20),
-    Sexe VARCHAR(10),
-    DataNaixement DATE, 
+    DNI VARCHAR(9) NOT NULL,
+    Nom VARCHAR(30) NOT NULL,
+    Cognom1 VARCHAR(30) NOT NULL,
+    Cognom2 VARCHAR(30) NOT NULL,
+    Nacionalitat VARCHAR(30) NOT NULL,
+    Sexe ENUM('H', 'D', 'NB', 'ND') NOT NULL,
+    DataNaixement DATE NOT NULL, 
     PRIMARY KEY(DNI), 
 );
 
+-- Creació de la taula Equip_Nacional
+CREATE TABLE IF NOT EXISTS Equip_Nacional (
+    Any INT(4), 
+    Pais VARCHAR(30),
+    PRIMARY KEY (Any, Pais)
+);    
