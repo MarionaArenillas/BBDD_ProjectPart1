9cc1f907ed99f3251a6cac100c877ed11705d0c2
MarionaArenillas <mariona.arenillas@gracia.lasalle.cat>
2025-02-06 12:36:00 +0100

    Creació taula conferencia i taula franquicia

--- exercici1.sql
+++ exercici1.sql
@@ -7,6 +7,8 @@ CREATE DATABASE IF NOT EXISTS LSG_NBA
 DEFAULT CHARACTER SET 'utf8mb4' 
 DEFAULT COLLATE 'utf8mb4_spanish_ci'; 
 
+USE LSG_NBA;
+
 -- Creació de la taula Persona: 
 CREATE TABLE IF NOT EXISTS Persona (
     DNI VARCHAR(9) NOT NULL,
@@ -30,4 +32,25 @@ CREATE TABLE IF NOT EXISTS Equip_Nacional (
 CREATE TABLE IF NOT EXISTS Draft (
     Any_draft INT(4),
     PRIMARY KEY(Any_draft)
+);
+
+-- Creació de la taula Conferència
+CREATE TABLE IF NOT EXISTS Conferència (
+    Nom_Conferencia VARCHAR(30),
+    ZonaGeografica VARCHAR(30) UNIQUE, 
+    PRIMARY KEY(Nom_Conferencia)
+);    
+
+-- Creació de la taula franquicia
+CREATE TABLE IF NOT EXISTS Franquicia (
+    Nom_franquicia VARCHAR(30), 
+    Ciutat_franquicia VARCHAR(30),
+    Pressupost_franquicia DECIMAL(15,2)
+    AnellsNBA INT(4),
+    DNI_Entrenador_Principal VARCHAR(9), 
+    Nom_Pavello VARCHAR(30), 
+    DNI_Propietari VARCHAR(9),
+    Nom_Conferencia VARCHAR(30),
+    PRIMARY KEY(Nom_franquicia),
+
 );
